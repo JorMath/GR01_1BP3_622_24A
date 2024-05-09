4ea5ce9ae1b25144c60c00158e497f7e53979132
JorMath <94020880+JorMath@users.noreply.github.com>
2024-05-09 17:02:59 -0500

    Segunda prueba unitaria sustracción

--- src/main/java/ec/edu/epn/git/calculator/Calculator.java
+++ src/main/java/ec/edu/epn/git/calculator/Calculator.java
@@ -2,30 +2,33 @@ package ec.edu.epn.git.calculator;
 
 public class Calculator {
     private int answer;
-    public int adicion(int a, int b) {
+
+    public int addition(int a, int b) {
         return a + b;
     }
-    public int subtract(int a, int b) {
+
+    public int subtraction(int a, int b) {
         return a - b;
     }
-    public int multiply(int a, int b) {
-        return a * b;
-    }
+
     @SuppressWarnings("IntegerDivisionInFloatingPointContext")
-    public int divide(int a, int b) {
+    public double division(int a, int b) {
         return a / b;
     }
-    public void timeout(int time){
-        try{
+
+    public void timeout(int time) {
+        try {
             Thread.sleep(time);
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    public int getAnswer(){
+
+    public int getAnswer() {
         return answer;
     }
-    public void setAnswer(int answer){
+
+    public void setAnswer(int answer) {
         this.answer = answer;
     }
 }
