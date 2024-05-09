f908a6d8de622d379856b31a1716b3a2d2250d57
JorMath <94020880+JorMath@users.noreply.github.com>
2024-05-09 16:20:23 -0500

    Segunda prueba unitaria sustracción

--- src/test/java/ec/edu/epn/git/calculator/CalculatorTest.java
+++ src/test/java/ec/edu/epn/git/calculator/CalculatorTest.java
@@ -10,4 +10,9 @@ public class CalculatorTest {
         Calculator calc = new Calculator();
         assertEquals(6,calc.adicion(4,2));
     }
+    @Test
+    public void given_two_integers_when_subtraction_then_ok(){
+        Calculator calc = new Calculator();
+        assertEquals(5,calc.subtract(10,5));
+    }
 }
